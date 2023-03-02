a81cd403e4010b14880c8ad5f164de0989b47b43
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-03-02 11:11:08 +0600

    chat backend up

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const app = express();
 
 const http = require('http').Server(app);
 const cors = require('cors');
+const { users } = require('./lib/users');
+const { listComments, createComment } = require('./lib/comments');
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/users', (req, res) => {
-	res.json();
+	res.json(users);
 });
 app.get('/comments', (req, res) => {
-	res.json();
+	const comments = listComments();
+	res.json(comments);
 });
-app.post('/comments', async (req, res) => {
-	res.json();
+app.post('/comment', async (req, res) => {
+	const comment = createComment();
+	res.json(comment);
 });
 
 http.listen(PORT, () => {
