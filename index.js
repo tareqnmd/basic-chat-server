b73920e2daa491861093b290b620ef83c1ae14b9
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-03-02 11:51:14 +0600

    up

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const http = require('http').Server(app);
 const cors = require('cors');
@@ -9,6 +10,22 @@ const { listComments, createComment } = require('./lib/comments');
 app.use(cors());
 app.use(express.json());
 
+// Add this to where your other requires are
+const io = require('socket.io')(http, {
+	cors: {
+		origin: 'http://localhost:3000',
+	},
+});
+
+// Above our `app.get("/users")` handler
+io.on('connection', (socket) => {
+	console.log(`⚡: ${socket.id} user just connected!`);
+
+	socket.on('disconnect', () => {
+		console.log('🔥: A user disconnected');
+	});
+});
+
 app.get('/users', (req, res) => {
 	res.json(users);
 });
@@ -16,10 +33,13 @@ app.get('/comments', (req, res) => {
 	const comments = listComments();
 	res.json(comments);
 });
-app.post('/comment', async (req, res) => {
-	const comment = createComment();
+app.post('/comments', async (req, res) => {
+	const comment = createComment(req.body);
+	io.emit('new-comment', { comment });
 	res.json(comment);
 });
+
+const PORT = process.env.PORT;
 
 http.listen(PORT, () => {
 	console.log(`Running at ${PORT}`);
