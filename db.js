6fa4f11c90ef56d5d3fd1a9b2f4b860d7fc2e52b
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-03-05 14:41:57 +0600

    up

---
+++ db.js
@@ -0,0 +1,18 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =
+	'mongodb://tareqnmd:tareqnmd@office@tareqnmd.t06grga.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+	const collection = client.db('chitchat').collection('users');
+	console.log('collection,err', collection, err);
+	app.post('/user', (req, res) => {
+		const user = getUser(req.body);
+		res.json(user);
+	});
+	client.close();
+});
